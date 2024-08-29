ff1f100c3ea3355fd7e29ce43ed69c2eb1810200
Abdou-Jabar Mikailou <151407343+Abdou-Jabar@users.noreply.github.com>
2024-08-29 19:16:24 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const nombreMystere = Math.floor(Math.random() * 100) + 1;
+
+let tentatives = 5;
+let essai = 0
+
+function devinerNombre() {
+    const saisie = document.getElementById("nombresaisie").value;
+    const nombreDevine = parseInt(saisie, 10);
+
+    let message = "";
+
+    if (isNaN(nombreDevine)) {
+        message = "Veuillez entrer un nombre valide.";
+    } else {
+        tentatives--;
+        essai
+        if (nombreDevine === nombreMystere) {
+            message = `Bravo! Vous avez deviné le nombre.`;
+            tentatives = 0; // Fin du jeu
+        } else if (nombreDevine < nombreMystere) {
+            message = "Trop petit! Essayez un nombre plus grand.";
+        } else if (nombreDevine > nombreMystere) {
+            message = "Trop grand! Essayez un nombre plus petit.";
+        }
+
+        if (tentatives === 0 && nombreDevine !== nombreMystere) {
+            redemarrer();
+        }
+    }
+
+    document.getElementById("message").innerText = message;
+    document.getElementById("tentative").innerText = `Vous avez ${tentatives} tentatives restantes.`;
+}
+
+function redemarrer() {
+    alert("Nombre de tentatives épuisé. Un nouveau nombre mystère a été choisi.");
+    tentatives = 5;
+    document.getElementById("message").innerText = "";
+    document.getElementById("tentative").innerText = `Vous avez ${tentatives} tentatives restantes.`;
+    // Générer un nouveau nombre mystère
+    nombreMystere = Math.floor(Math.random() * 100) + 1;
+}
